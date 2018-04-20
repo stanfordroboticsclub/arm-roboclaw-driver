cb56d052ae1c0994fb99a96aeb608e4592b9fbea
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-04-19 17:08:56 -0700

    Added diagnostics functions

--- scripts/roboclaw_driver.py
+++ scripts/roboclaw_driver.py
@@ -752,19 +752,19 @@ def LeftRightMixed(address, val):
     return _write1(address, Cmd.MIXEDLR, val)
 
 
-def ReadEncM1(address):
+# def ReadEncM1(address):
     return _read4_1(address, Cmd.GETM1ENC)
 
 
-def ReadEncM2(address):
+# def ReadEncM2(address):
     return _read4_1(address, Cmd.GETM2ENC)
 
 
-def ReadSpeedM1(address):
+# def ReadSpeedM1(address):
     return _read4_1(address, Cmd.GETM1SPEED)
 
 
-def ReadSpeedM2(address):
+# def ReadSpeedM2(address):
     return _read4_1(address, Cmd.GETM2SPEED)
 
 
@@ -772,7 +772,7 @@ def ResetEncoders(address):
     return _write0(address, Cmd.RESETENC)
 
 
-def ReadVersion(address):
+# def ReadVersion(address):
     global _crc
     trys = _trystimeout
     while 1:
@@ -804,19 +804,19 @@ def ReadVersion(address):
     return 0, 0
 
 
-def SetEncM1(address, cnt):
+# def SetEncM1(address, cnt):
     return _write4(address, Cmd.SETM1ENCCOUNT, cnt)
 
 
-def SetEncM2(address, cnt):
+# def SetEncM2(address, cnt):
     return _write4(address, Cmd.SETM2ENCCOUNT, cnt)
 
 
-def ReadMainBatteryVoltage(address):
+# def ReadMainBatteryVoltage(address):
     return _read2(address, Cmd.GETMBATT)
 
 
-def ReadLogicBatteryVoltage(address, ):
+# def ReadLogicBatteryVoltage(address, ):
     return _read2(address, Cmd.GETLBATT)
 
 
@@ -924,7 +924,7 @@ def ReadPWMs(address):
     return 0, 0, 0
 
 
-def ReadCurrents(address):
+# def ReadCurrents(address):
     val = _read4(address, Cmd.GETCURRENTS)
     if val[0]:
         cur1 = val[1] >> 16
@@ -1098,7 +1098,7 @@ def RestoreDefaults(address):
     return _write0(address, Cmd.RESTOREDEFAULTS)
 
 
-def ReadTemp(address):
+# def ReadTemp(address):
     return _read2(address, Cmd.GETTEMP)
 
 
@@ -1106,7 +1106,7 @@ def ReadTemp2(address):
     return _read2(address, Cmd.GETTEMP2)
 
 
-def ReadError(address):
+# def ReadError(address):
     return _read2(address, Cmd.GETERROR)
 
 
@@ -1147,11 +1147,11 @@ def GetConfig(address):
     return _read2(address, Cmd.GETCONFIG)
 
 
-def SetM1MaxCurrent(address, max):
+# def SetM1MaxCurrent(address, max):
     return _write44(address, Cmd.SETM1MAXCURRENT, max, 0)
 
 
-def SetM2MaxCurrent(address, max):
+# def SetM2MaxCurrent(address, max):
     return _write44(address, Cmd.SETM2MAXCURRENT, max, 0)
 
 
@@ -1177,7 +1177,7 @@ def ReadPWMMode(address):
     return _read1(address, Cmd.GETPWMMODE)
 
 
-def Open(comport, rate):
+# def Open(comport, rate):
     global port
     port = serial.Serial(comport, baudrate=rate, timeout=0.1, interCharTimeout=0.01)
     return
--- scripts/roboclaw_interface.py
+++ scripts/roboclaw_interface.py
@@ -39,27 +39,51 @@ class RoboClaw:
         #         raise
 
 
-    def ReadVersion(self, motor):
+    def read_version(self, motor):
         return roboclaw_driver.ReadVersion(self.address[motor])
 
-    def ReadEncoder(self, motor):
+    def read_error(self,motor)
+        return roboclaw_driver.ReadError(self.address[motor])
+
+    def read_main_battery_voltage(self, motor):
+        return roboclaw_driver.ReadMainBatteryVoltage(self.address[motor]):
+        
+    def read_logic_battery_voltage(self, motor):
+        return roboclaw_driver.ReadLogicBatteryVoltage(self.address[motor]):
+
+    def read_temp(self,motor):
+        return roboclaw_driver.ReadTemp(self.address[motor])
+
+
+    def read_current(self,motor):
+        return roboclaw_driver.ReadCurrents(self.address[motor])[self.motor_num[motor]]
+
+    def set_max_current(self,motor,current):
+        if self.motor_num[motor] == 1:
+            out = roboclaw_driver.SetM1MaxCurrent(self.address[motor],current)
+        else:
+            out = roboclaw_driver.SetM2MaxCurrent(self.address[motor],current)
+        return out
+
+
+    def read_encoder(self, motor):
         if self.motor_num[motor] == 1:
             out = roboclaw_driver.ReadEncM1(self.address[motor])
         else:
             out = roboclaw_driver.ReadEncM2(self.address[motor])
         return out
 
-    def ReadEncoderSpeed(self, motor):
+    def read_encoder_speed(self, motor):
         if self.motor_num[motor] == 1:
             out = roboclaw_driver.ReadSpeedM1(self.address[motor])
         else:
             out = roboclaw_driver.ReadSpeedM2(self.address[motor])
         return out
 
-
-    def SetEncoder(self, motor, val):
+    def set_encoder(self, motor, val):
         if self.motor_num[motor] == 1:
             out = roboclaw_driver.SetEncM1(self.address[motor], val)
         else:
             out = roboclaw_driver.SetEncM2(self.address[motor], val)
         return out
+
