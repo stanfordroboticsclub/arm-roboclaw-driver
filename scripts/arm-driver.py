2fb83bada39953106836d14a7a3dd8149c42d2a9
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-05-10 21:04:22 -0700

    added wrist calibration

--- scripts/arm-driver.py
+++ scripts/arm-driver.py
@@ -17,32 +17,40 @@ class ArmDriver:
                               # "wrist_pitch",
                               # "grip"]
 
-        self.motor_names = ["shoulder",
-                              "elbow",
-                              "wrist_L",
+        self.motor_names = ["wrist_L",
                               "wrist_R"]
+
+        # self.motor_names = ["shoulder",
+        #                       "elbow",
+        #                       "wrist_L",
+        #                       "wrist_R"]
                               # "turret",
                               # "wrist_pitch",
                               # "grip"]
 
-        self.manual_names = ["shoulder",
-                              "elbow"]
+        self.manual_names = []
+        # self.manual_names = ["shoulder",
+        #                       "elbow"]
                               # "turret",
                               # "wrist_pitch",
                               # "grip"]
 
         self.pos = {'shoulder': 4, 
-                        'elbow': 4}
+                        'elbow': 4,
+                    'wrist_pitch': math.pi/2,
+                    'wrist_yaw': math.pi/2}
+                   
 
         self.convert = { "shoulder" : ( 3 ,4.6 , 150 ,1650),
                        "elbow" : (3.5 , 5.2 , 60, 1400),
-                       "wrists" : (-math.pi , math.pi , -8000 , 8000),
+                       "wrist_pitch" : (0 , math.pi , -10000 , 10000),
+                       "wrist_yaw" : (0 , math.pi , -10000 , 10000),
                        }
 
-        self.rc = RoboClaw(self.find_serial_port(), names = self.joint_names) # addresses = [128, 129, 130])
+        # self.rc = RoboClaw(self.find_serial_port(), names = self.motor_names) # addresses = [128, 129, 130])
+        self.rc = RoboClaw(self.find_serial_port(), names = self.motor_names,addresses = [129] ) # addresses = [128, 129, 130])
 
         rospy.init_node('arm-driver', anonymous=True)
-
         rospy.Subscriber("/joint_states", JointState, lambda x: self.callback(x) )
 
 
@@ -54,11 +62,8 @@ class ArmDriver:
 
 
     def callback(self, data):
-
         for joint in self.joint_names:
 
-            if joint not in ['shoulder', 'elbow']:
-                continue
             try:
                 ind = data.name.index(joint)
             except ValueError:
@@ -71,12 +76,20 @@ class ArmDriver:
             position = self.scale(self.pos.[joint], joint)
             self.rc.drive_position(joint, position)
 
+        pulse_pitch = self.scale(self.pos.['wrist_pitch'], 'wrist_pitch')
+        pulse_yaw = self.scale(self.pos.['wrist_yaw'], 'wrist_yaw')
 
-        wrist_L_pos = self.pos[wrist_pitch] + self.pos[wrist_yaw] - math.pi
-        wrist_R_pos = self.pos[wrist_pitch] - self.pos[wrist_yaw]
+        wrist_L_pulse = pulse_pitch + pulse_yaw
+        wrist_R_pulse = pulse_pitch - pulse_yaw
 
-        self.rc.drive_position(joint, position)
-        self.rc.drive_position(joint, position)
+        wrist_L_pulse = self.clamp(wrist_L_pulse, -10000,10000)
+        wrist_R_pulse = self.clamp(wrist_R_pulse, -10000,10000)
+
+        self.rc.drive_position('wrist_L', wrist_L_pulse)
+        self.rc.drive_position('wrist_R', wrist_R_pulse)
+
+    def clamp(self,val, mi, ma):
+        return min( [ max( [val,mi] ), ma])
 
             
     def scale(self, x, joint):
