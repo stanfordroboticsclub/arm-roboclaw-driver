4a2445722b653add4db999f748810c167322ade9
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-05-16 20:43:11 -0700

    broke read version but hey drive duty is more reilable

--- arduino/ServoDriver/ServoDriver.ino
+++ arduino/ServoDriver/ServoDriver.ino
@@ -43,51 +43,79 @@ void setup() {
 
 
 void loop() {
-  while( Serial1.available() ){
-    buffer[i] = Serial1.read();
-    i++;
-    if(i>9){
-      while( Serial1.available() ) Serial1.read();
-      i=0;
-      break;
-    }}
-    
-
-    if(i>0){
-
-       if(i<2) i=2;
-       uint16_t ch = crc16(buffer, i-2);
-//       unsigned int ch = crc16(buffer, 4);
-       
-       if(buffer[0] == ADDRESS &&
-          buffer[1] == M1DUTYCOMMAND &&
-          ( ((uint16_t)buffer[4])<<8 | ((uint16_t)buffer[5])) == ch)
-          {
-            target = buffer[2]<<8 | buffer[3];
-            Serial1.write(255);
-          }  
-          else if(buffer[0] == ADDRESS &&
-          buffer[1] == READVERSION &&
-          ( ((uint16_t)buffer[2])<<8 | ((uint16_t)buffer[4])) == ch)
-          
-          {
-
-            
-            
-            uint16_t verify = crc16( (unsigned char*)version_string, strlen(version_string)+1);
-
-            Serial.println(verify);
-            Serial1.print(version_string+2);
-            Serial1.write(0);
-            Serial1.write((char)(verify >> 8));
-            Serial1.write((char)verify);
-          }
 
 
-          
-          i = 0;
-    }
-    
+while (Serial1.available() ){
+      unsigned char address = Serial1.read();
+      Serial.print("Got address: "); Serial.println(address);
+
+      if(address == ADDRESS){
+        unsigned char command = Serial1.read();
+
+        Serial.print("Got command: "); Serial.println(command);
+
+
+        if(command == M1DUTYCOMMAND){
+
+              if(!Serial1.available()) break;
+              unsigned char high_val = Serial1.read();
+              
+              if(!Serial1.available()) break;
+              unsigned char low_val = Serial1.read();
+
+              if(!Serial1.available()) break;
+              unsigned char high_crc = Serial1.read();
+              
+              if(!Serial1.available()) break;
+              unsigned char low_crc = Serial1.read();
+
+              buffer[0] = address;
+              buffer[1] = command;
+              buffer[2] = high_val;
+              buffer[3] = low_val;
+
+              uint16_t ch = crc16(buffer, 4);
+              if(( ( ( ((uint16_t)high_crc)<<8) )  | ((uint16_t)low_crc) ) == ch){
+                target = high_val<<8 | low_val;
+              }
+
+        }else if(command == READVERSION){
+
+      
+              if(!Serial1.available()) break;
+              unsigned char high_crc = Serial1.read();
+              
+              if(!Serial1.available()) break;
+              unsigned char low_crc = Serial1.read();
+
+              Serial.print("Got high: "); Serial.println(high_crc);
+              Serial.print("Got low : "); Serial.println(low_crc);
+
+              buffer[0] = address;
+              buffer[1] = command;
+
+              uint16_t ch = crc16(buffer, 2);
+
+              if(( ( ( ((uint16_t)high_crc)<<8) )  | ((uint16_t)low_crc) ) == ch){
+                
+                    uint16_t verify = crc16( (unsigned char*)version_string, strlen(version_string)+1);
+        
+                    Serial.println(verify);
+                    Serial1.print(version_string+2);
+                    Serial1.write(0);
+                    Serial1.write((char)(verify >> 8));
+                    Serial1.write((char)verify);
+                    Serial.println("version command");
+                
+              }
+              
+        }
+
+      }  
+}
+
+
+
 
 
   if(val!=target){
