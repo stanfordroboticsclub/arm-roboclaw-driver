9b81acbb0c75c3bd718f19323c4ba919344a02df
Michal <mikh3x4@gmail.com>
2018-05-16 20:45:16 -0700

    fixed gripper bugs

--- scripts/arm-driver.py
+++ scripts/arm-driver.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-
+from __future__ import division
 
 import rospy
 from sensor_msgs.msg import JointState
 from roboclaw_interface import RoboClaw
 import math
 import time
+
 
 class ArmDriver:
 
@@ -50,13 +51,14 @@ class ArmDriver:
                     'wrist_yaw': 0,
                        'grip':0}
         
+        self.last_grip = 0
                    
 
         self.convert = { "shoulder" : ( 3 ,4.6 , 150 ,1650),
                        "elbow" : (3.5 , 5.2 , 60, 1400),
                        "wrist_pitch" : (0 , math.pi , -10000 , 10000),
                        "wrist_yaw" : (0 , math.pi , -10000 , 10000),
-                       "grip" : (1 , 0.5 , 120 , 50)
+                       "grip" : (0.5 , 1 , 50 , 120)
                        }
 
         # self.rc = RoboClaw(self.find_serial_port(), names = self.motor_names) # addresses = [128, 129, 130])
@@ -70,7 +72,7 @@ class ArmDriver:
         rospy.Subscriber("/joint_states", JointState, lambda x: self.callback(x) )
 
 
-        r = rospy.Rate(10)
+        r = rospy.Rate(5)
         while not rospy.is_shutdown():
             rospy.loginfo('new loop')
         # while 1:
@@ -81,7 +83,7 @@ class ArmDriver:
 
 
     def callback(self, data):
-        rospy.loginfo('callback')
+        # rospy.loginfo('callback')
         for joint in self.joint_names:
 
             try:
@@ -93,7 +95,7 @@ class ArmDriver:
             self.offset[joint] = int(data.effort[ind])
 
         
-        rospy.loginfo('call '+str(self.pos['wrist_pitch']) )
+        # rospy.loginfo('call '+str(self.pos['wrist_pitch']) )
 
     def write_to_roboclaw(self):
         rospy.loginfo('writing')
@@ -113,14 +115,26 @@ class ArmDriver:
         wrist_L_pulse = self.clamp(wrist_L_pulse, -10000,10000) + self.offset[ 'wrist_pitch']
         wrist_R_pulse = self.clamp(wrist_R_pulse, -10000,10000) + self.offset[ 'wrist_yaw']
 
+
+        # print "XXXX WRIST L", wrist_L_pulse
+        # print "XXXX WRIST R", wrist_R_pulse
+
         out = self.rc.drive_position('wrist_L', wrist_L_pulse)
         out2 = self.rc.drive_position('wrist_R', wrist_R_pulse)
 
 
         #DO MANUAL for Grip
+        print "SCLAE" , self.pos['grip']
         position = self.scale(self.pos['grip'], 'grip')
         offset = self.offset['grip']
-        # self.rc.drive_duty('grip', position + offset)
+
+        print "POS", position
+        print "GRIP", position + offset
+
+        if(self.last_grip != position + offset):
+            print "CAHNGED"
+            self.last_grip = position + offset
+        self.rc.drive_duty('grip', position + offset)
 
 
         rospy.loginfo('confirmation')
